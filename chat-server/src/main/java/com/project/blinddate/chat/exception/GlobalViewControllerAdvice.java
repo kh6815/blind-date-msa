39ff9d9854e7b0a75170bc56e9e5984a03011578
choeganghyeon <kh6815@naver.com>
2026-03-10 08:18:13 +0900

    1. https key 파일 위치 수정

--- chat-server/src/main/java/com/project/blinddate/chat/exception/GlobalViewControllerAdvice.java
+++ chat-server/src/main/java/com/project/blinddate/chat/exception/GlobalViewControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.project.blinddate.chat.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,17 +11,22 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @ControllerAdvice(annotations = Controller.class)
 public class GlobalViewControllerAdvice {
 
+    @Value("${external.user-server.url}")
+    private String externalUserServerUrl;
+
     @ExceptionHandler(IllegalArgumentException.class)
     public String handleIllegalArgumentException(IllegalArgumentException e, RedirectAttributes redirectAttributes) {
         log.warn("View controller illegal argument exception: {}", e.getMessage());
         redirectAttributes.addFlashAttribute("error", e.getMessage());
-        return "redirect:/chats"; // Redirect to chat list
+//        return "redirect:/chats"; // Redirect to chat list
+        return "redirect:" + externalUserServerUrl + "/login";
     }
 
     @ExceptionHandler(Exception.class)
     public String handleException(Exception e, RedirectAttributes redirectAttributes) {
         log.error("View controller internal server error: ", e);
         redirectAttributes.addFlashAttribute("error", "알 수 없는 오류가 발생했습니다.");
-        return "redirect:/chats";
+//        return "redirect:/chats";
+        return "redirect:" + externalUserServerUrl + "/login";
     }
 }
